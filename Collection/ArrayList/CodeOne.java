793b137a3758ea506c6fb7b4da8d50451e768b2c
Nayan <nayandahare02@gmail.com>
2026-04-13 23:09:08 +0530

    Initial commit

---
+++ Collection/ArrayList/CodeOne.java
@@ -0,0 +1,18 @@
+package Collection.ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class CodeOne{
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        System.out.println("List is " + list);
+        System.out.println("Size is " + list.size());
+
+    }
+}
