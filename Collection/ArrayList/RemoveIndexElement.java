73c7ceeb799ebe3c39d36dc8bb628992c61129f6
Nayan <nayandahare02@gmail.com>
2026-04-13 23:59:56 +0530

    RemoveIndexElement

--- Collection/ArrayList/RemoveIndexElement.java
+++ Collection/ArrayList/RemoveIndexElement.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class RemoveIndexElement {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        List<Integer> list = new ArrayList<Integer>();
+        for(int i = 1 ; i <= 10 ; i++)
+        {
+            list.add(i);
+        }
+
+        System.out.println("List is ");
     }
 }
